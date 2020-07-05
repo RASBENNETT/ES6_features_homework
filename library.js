aae134e3c2525d2c0274848973d13a3eb7689878
Ruairidh Benentt <ruairidhbennett@gmail.com>
2020-07-05 22:11:46 +0100

    Initial Commit

---
+++ library.js
@@ -0,0 +1,28 @@
+class Library {
+  constructor(books = []){
+    this.books = books;
+  }
+
+  bookCount = function () {
+    return this.books.length;
+  };
+
+  addBook = function (newBook) {
+    this.books.push(newBook);
+  };
+
+  addBooks = function (newBooks) {
+    newBooks.forEach(book => this.books.push(book));
+  };
+
+  printInventory = function () {
+    this.books.forEach((book) => {
+      console.log(`${book.title} by ${book.author}`);
+    });
+  };
+}
+
+export default Library
+
+
+// module.exports = Library;
